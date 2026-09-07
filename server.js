ab61c83cb988a2fd0bb6a1d44d1c818e68eed0e2
Hamid00Fathi <166663270+Hamid00Fathi@users.noreply.github.com>
2026-09-08 01:12:42 +0330

    Update server.js

--- server.js
+++ server.js
@@ -64,12 +64,3 @@ app.get("/status/:username", async (req, res) => {
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
-
-app.get("/status/delete_all", async (req, res) => {
-    try {
-        await User.deleteMany({});
-        res.json({ ok: true, message: "تمام کاربران حذف شدند" });
-    } catch (err) {
-        res.status(500).json({ error: err.toString() });
-    }
-});
